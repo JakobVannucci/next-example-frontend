f0e82ed9beb6906aec0db498d6a8fb9e2301a598
Jakob Vannucci <jakob.vannucci@protestsoftware.com.au>
2022-01-20 12:11:29 +1000

    FIx use auth to use new SWR config

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,115 +1,120 @@
-import useSWR from 'swr'
+import useSWR, { useSWRConfig } from 'swr'
 import axios from '@/lib/axios'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
-    const router = useRouter()
+  const router = useRouter()
 
-    const { data: user, error, revalidate } = useSWR('/api/user', () =>
-        axios
-            .get('/api/user')
-            .then(res => res.data)
-            .catch(error => {
-                if (error.response.status != 409) throw error
+  const { mutate } = useSWRConfig()
 
-                router.push('/verify-email')
-            }),
-    )
+  const { data: user, error } = useSWR('/api/user', () =>
+    axios
+      .get('/api/user')
+      .then(res => res.data)
+      .catch(error => {
+        if (error.response.status != 409) throw error
 
-    const csrf = () => axios.get('/sanctum/csrf-cookie')
+        router.push('/verify-email')
+      }),
+  )
 
-    const register = async ({ setErrors, ...props }) => {
-        await csrf()
+  const csrf = () => axios.get('/sanctum/csrf-cookie')
 
-        setErrors([])
+  const register = async ({ setErrors, ...props }) => {
+    await csrf()
 
-        axios
-            .post('/register', props)
-            .then(() => revalidate())
-            .catch(error => {
-                if (error.response.status != 422) throw error
+    setErrors([])
 
-                setErrors(Object.values(error.response.data.errors).flat())
-            })
+    axios
+      .post('/register', props)
+      .then(() => mutate('/api/user'))
+      .catch(error => {
+        if (error.response.status != 422) throw error
+
+        setErrors(Object.values(error.response.data.errors).flat())
+      })
+  }
+
+  const login = async ({ setErrors, setStatus, ...props }) => {
+    await csrf()
+
+    setStatus(null)
+    setErrors([])
+
+    axios
+      .post('/login', props)
+      .then(() => mutate('/api/user'))
+      .catch(error => {
+        if (error.response.status != 422) throw error
+
+        setErrors(Object.values(error.response.data.errors).flat())
+      })
+  }
+
+  const forgotPassword = async ({ setErrors, setStatus, email }) => {
+    await csrf()
+
+    setStatus(null)
+    setErrors([])
+
+    axios
+      .post('/forgot-password', { email })
+      .then(response => setStatus(response.data.status))
+      .catch(error => {
+        if (error.response.status != 422) throw error
+
+        setErrors(Object.values(error.response.data.errors).flat())
+      })
+  }
+
+  const resetPassword = async ({ setErrors, setStatus, ...props }) => {
+    await csrf()
+
+    setStatus(null)
+    setErrors([])
+
+    axios
+      .post('/reset-password', { token: router.query.token, ...props })
+      .then(response =>
+        router.push('/login?reset=' + btoa(response.data.status)),
+      )
+      .catch(error => {
+        if (error.response.status != 422) throw error
+
+        setErrors(Object.values(error.response.data.errors).flat())
+      })
+  }
+
+  const resendEmailVerification = ({ setStatus }) => {
+    axios
+      .post('/email/verification-notification')
+      .then(response => setStatus(response.data.status))
+  }
+
+  const logout = async () => {
+    if (!error) {
+      await axios.post('/logout')
+
+      mutate('/api/user')
     }
 
-    const login = async ({ setErrors, setStatus, ...props }) => {
-        await csrf()
+    window.location.pathname = '/login'
+  }
 
-        setStatus(null)
-        setErrors([])
+  useEffect(() => {
+    if (middleware == 'guest' && redirectIfAuthenticated && user)
+      router.push(redirectIfAuthenticated)
+    if (middleware == 'auth' && error) logout()
+  }, [user, error])
 
-        axios
-            .post('/login', props)
-            .then(() => revalidate())
-            .catch(error => {
-                if (error.response.status != 422) throw error
-
-                setErrors(Object.values(error.response.data.errors).flat())
-            })
-    }
-
-    const forgotPassword = async ({ setErrors, setStatus, email }) => {
-        await csrf()
-
-        setStatus(null)
-        setErrors([])
-
-        axios
-            .post('/forgot-password', { email })
-            .then(response => setStatus(response.data.status))
-            .catch(error => {
-                if (error.response.status != 422) throw error
-
-                setErrors(Object.values(error.response.data.errors).flat())
-            })
-    }
-
-    const resetPassword = async ({ setErrors, setStatus, ...props }) => {
-        await csrf()
-
-        setStatus(null)
-        setErrors([])
-
-        axios
-            .post('/reset-password', { token: router.query.token, ...props })
-            .then(response => router.push('/login?reset=' + btoa(response.data.status)))
-            .catch(error => {
-                if (error.response.status != 422) throw error
-
-                setErrors(Object.values(error.response.data.errors).flat())
-            })
-    }
-
-    const resendEmailVerification = ({ setStatus }) => {
-        axios
-            .post('/email/verification-notification')
-            .then(response => setStatus(response.data.status))
-    }
-
-    const logout = async () => {
-        if (! error) {
-            await axios.post('/logout')
-
-            revalidate()
-        }
-
-        window.location.pathname = '/login'
-    }
-
-    useEffect(() => {
-        if (middleware == 'guest' && redirectIfAuthenticated && user) router.push(redirectIfAuthenticated)
-        if (middleware == 'auth' && error) logout()
-    }, [user, error])
-
-    return {
-        user,
-        register,
-        login,
-        forgotPassword,
-        resetPassword,
-        resendEmailVerification,
-        logout,
-    }
+  return {
+    user,
+    register,
+    login,
+    forgotPassword,
+    resetPassword,
+    resendEmailVerification,
+    logout,
+  }
 }
